48268c8d9a16009ceb26c177f96aea5ae8194f91
Muhammad Abdullah <76752572+dexter845@users.noreply.github.com>
2024-06-04 07:34:51 +0500

    build app

---
+++ app/routes/test.tsx
@@ -0,0 +1,10 @@
+export default function Test() {
+  return (
+    <p id="index-page">
+      This is a test page for remix
+      <br />
+      Check out{" "}
+      <a href="https://remix.run">the docs at remix.run</a>.
+    </p>
+  );
+}
